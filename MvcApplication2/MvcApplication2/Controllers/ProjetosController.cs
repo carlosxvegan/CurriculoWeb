9e101c241feb4267f6bc60f5291b889710a233dd
Carlos souza <carlos.henrike13@gmail.com>
2015-03-10 16:12:14 -0300

    001

---
+++ MvcApplication2/MvcApplication2/Controllers/ProjetosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class ProjetosController : Controller
+    {
+        //
+        // GET: /Projetos/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
